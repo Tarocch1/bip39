fa328272e4aa474d3d210be792b7da29d6c3f165
Tarocch1 <huiben.fang@gmail.com>
2026-02-28 18:56:45 +0800

    feat: update

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,7 @@
 import antfu from '@antfu/eslint-config'
 import { option } from '@tarocch1/eslint-config'
 
-export default antfu(option({
+export default antfu(...option({
   rules: {
     'node/prefer-global/buffer': ['off'],
   },
